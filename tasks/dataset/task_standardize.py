85de2aee1d3e7988fda8764b08d1c71728830f87
test-dan-run <daniel.leongsj@gmail.com>
2022-02-28 15:19:17 +0800

    update clearml standardization code

--- tasks/dataset/task_standardize.py
+++ tasks/dataset/task_standardize.py
@@ -1,7 +1,9 @@
 from clearml import Task, StorageManager, Logger, Dataset
 
-PROJECT_NAME = "xxx"
+PROJECT_NAME = "audio_preproc_test"
 TASK_NAME = "audio_standardizing"
+DATASET_NAME = "librispeech_small_standardized"
+OUTPUT_URL = "s3://experiment-logging/storage"
 
 task = Task.init(project_name=PROJECT_NAME, task_name=TASK_NAME)
 task.set_base_docker(
@@ -11,16 +13,39 @@ task.set_base_docker(
 
 args = {
     'dataset_task_id': '',
+    'input_filetype': '',
+    'normalize': None,
     'sample_rate': None,
     'channels': None,
-    'normalize': None,
 }
 
 task.connect(args)
 task.execute_remotely()
 
-# xxx
-
 from preprocessing.standardize import standardize
+
+# import dataset
+
+dataset = Dataset.get(dataset_id = args['dataset_task_id'])
+dataset_path = dataset.get_local_copy()
+
+# process
+new_dataset_path = standardize(
+    input_dir = dataset_path,
+    input_filetype = args['input_filetype'],
+    normalize = args['normalize'],
+    sample_rate = args['sample_rate'],
+    channels = args['channels']
+)
+
+# register ClearML Dataset
+dataset = Dataset.create(
+    dataset_project=PROJECT_NAME, dataset_name=DATASET_NAME
+)
+dataset.add_files(new_dataset_path)
+dataset.upload(output_url=OUTPUT_URL)
+dataset.finalize()
+
+print('Done')
 
 # yyy
