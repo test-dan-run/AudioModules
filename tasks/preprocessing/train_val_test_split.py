c08cc969f7ef9358e39ed28e7f77d5b441bcaee5
test-dan-run <daniel.leongsj@gmail.com>
2022-04-18 16:50:26 +0800

    add train dev test split functionality

--- tasks/preprocessing/train_val_test_split.py
+++ tasks/preprocessing/train_val_test_split.py
@@ -1,7 +1,8 @@
 import os
+import math
 import json
 import random
-from typing import Iterable
+from typing import Iterable, List
 
 class DatasetSplitter():
     def __init__(
@@ -17,7 +18,43 @@ class DatasetSplitter():
         assert len(split_ratio) == len(split_names), \
              'no. of floats in split_ratio should correspond to the no. of names in split_names'
 
-        self.split_ratio = split_ratio
+        assert math.fsum(split_ratio) == 1.0, 'split_ratio should sum up to 1'
+
+        self.split_boundary = []
+        for idx in range(len(split_ratio)):
+            self.split_boundary.append(sum(split_ratio[:idx+1]))
         self.split_names = split_names        
         random.seed(random_seed)
 
+    def split_manifest(
+        self,
+        input_manifest_path: str,
+        ) -> List[str]:
+
+        data_splits = [[] for _ in range(len(self.split_boundary))]
+        with open(input_manifest_path, mode='r', encoding='utf-8') as f:
+
+            lines = f.readlines()
+            for line in lines:
+                random_val = random.random()
+                for idx, boundary in enumerate(self.split_boundary):
+                    if random_val < boundary:
+                        data_splits[idx].append(json.loads(line))
+                        break
+        
+        file_paths = []
+        for name, data_split in zip(self.split_names, data_splits):
+            file_path = os.path.join(name + '_' + os.path.basename(input_manifest_path))
+            with open(file_path, mode='w', encoding='utf-8') as f:
+                for item in data_split:
+                    f.write(json.dumps(item) + '\n')
+
+            file_paths.append(file_path)
+
+        return file_paths
+
+
+if __name__ == '__main__':
+    INPUT_MANIFEST_PATH = '/home/daniel/datasets/emotion_sd/manifest.json'
+    s = DatasetSplitter(split_ratio=(0.7, 0.2, 0.1), split_names=('train', 'dev', 'test'), random_seed=42)
+    s.split_manifest(INPUT_MANIFEST_PATH)
---
+++ tasks/task_train_val_test_split.py
@@ -0,0 +1,60 @@
+from clearml import Task, Dataset, TaskTypes
+
+PROJECT_NAME = "audio_preproc_test"
+TASK_NAME = "dataset_split"
+DATASET_POSTFIX = "_split"
+OUTPUT_URL = "s3://experiment-logging/storage"
+
+task = Task.init(project_name=PROJECT_NAME, task_name=TASK_NAME, task_type=TaskTypes.data_processing)
+task.set_base_docker(docker_image="dleongsh/audio_preproc:v1.0.0")
+
+# librispeech_small dataset_task_id: 092896c34c0e45b598777222d9eaaee6
+args = {
+    'dataset_task_id': '',    
+    'split_names': ('train', 'dev', 'test'),
+    'split_ratio': (0.8, 0.1, 0.1),
+    'random_seed': 42
+}
+
+task.connect(args)
+task.execute_remotely()
+
+import os
+from preprocessing.train_val_test_split import DatasetSplitter
+
+# import parent dataset's manifest file
+parent_dataset = Dataset.get(dataset_id = args['dataset_task_id'])
+input_manifest_path = parent_dataset.artifacts['manifest.json'].get_local_copy()
+
+# initialize class object
+ds_splitter = DatasetSplitter(
+    split_ratio=args['split_ratio'],
+    split_names=args['split_names'],
+    random_seed=args['random_seed']
+)
+
+# run splitter
+output_manifest_paths = ds_splitter.split_manifest(input_manifest_path=input_manifest_path)
+
+# register ClearML Dataset
+clearml_dataset = Dataset.create(
+    dataset_project=parent_dataset.project, 
+    dataset_name=parent_dataset.name + DATASET_POSTFIX, 
+    parent_datasets=[args['dataset_task_id'],],
+)
+
+for path in output_manifest_paths:
+    # upload manifest files as artifacts
+    task.upload_artifact(name=os.path.basename(path), artifact_object=path)
+    # upload each individual manifest file
+    clearml_dataset.add_files(path)
+
+clearml_dataset.upload(output_url=OUTPUT_URL)
+clearml_dataset.finalize()
+
+task.set_parameter(
+    name='output_dataset_id', 
+    value=clearml_dataset.id, 
+    description='the dataset task id of the output dataset'
+    ) 
+print('Done')
