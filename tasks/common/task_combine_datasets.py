8036d2cfd9fd0d63c5927ced051f6c5ebc76aaed
test-dan-run <daniel.leongsj@gmail.com>
2022-04-25 11:36:12 +0800

    update combine datasets

--- tasks/common/task_combine_datasets.py
+++ tasks/common/task_combine_datasets.py
@@ -8,7 +8,7 @@ PROJECT_NAME = "audio_preproc_test"
 TASK_NAME = "combine_datasets"
 DATASET_PROJECT = 'datasets/sample_dataset'
 DATASET_NAME = 'test_audio_combined'
-ARTIFACT_NAMES = ['train_manifest.json', 'dev_manifest.json', 'test_manifest.json']
+ARTIFACT_NAMES = ['train_manifest', 'dev_manifest', 'test_manifest']
 
 # s3://<server url>:<port>/<bucket>/...
 OUTPUT_URI = 's3://experiment-logging/storage'
@@ -24,65 +24,114 @@ args = {
     'dataset_name': DATASET_NAME,
     'artifact_names': ARTIFACT_NAMES,
     'parent_dataset_ids': [],
+    'parent_dataset_projects': [],
+    'parent_dataset_names': [],
 }
 
 task.connect(args)
 task.execute_remotely()
 
-# intialize dataset task as current task
-dataset = Dataset.create(
-    dataset_project = args['dataset_project'],
-    dataset_name = args['dataset_name'],
-    parent_datasets = args['parent_dataset_ids'],
-)
-dataset_task = Task.get_task(task_id=dataset.id)
+def get_parent_ids_by_names(
+    parent_dataset_projects: List[str], 
+    parent_dataset_names: List[str]
+    ) -> List[str]:
 
-# pull artifacts from each parent dataset
-artifact_paths = {}
+    dataset_ids = []
 
-for parent_id in args['parent_dataset_ids']:
-    parent_dataset_task = Task.get_task(task_id=parent_id)
+    for project in parent_dataset_projects:
+        for name in parent_dataset_names:
+            dataset = Task.get_task(project_name=project, task_name=name)
+            if dataset is not None:
+                dataset_ids.append(dataset.id)
 
-    for artifact_name in args['artifact_names']:
-        if artifact_name not in artifact_paths:
-            artifact_paths[artifact_name] = []
-        # pull artifact and store in a list
-        artifact_path = parent_dataset_task.artifacts[artifact_name].get_local_copy()
-        # if file does not exist
-        if artifact_path is None:
-            continue
-        artifact_paths[artifact_name].append(artifact_path)
+    return dataset_ids
 
-# combine and upload artifacts
-for output_path in artifact_paths.keys():
-    print(f'Combining {len(artifact_paths[output_path])} files into: {output_path}')
-    with open(output_path, mode='w', encoding='utf-8') as fw:
-        # enter each input artifact and append each line to the new output artifact
-        for input_path in artifact_paths[output_path]:
-            with open(input_path, mode='r', encoding='utf-8') as fr:
-                lines = fr.readlines()
-                for line in lines:
-                    fw.write(line)
+def combine_datasets(
+    dataset_project: str,
+    dataset_name: str,
+    artifact_names: List[str] = None,
+    parent_dataset_ids: List[str] = [],
+    parent_dataset_projects: List[str] = [],
+    parent_dataset_names: List[str] = []
+    ) -> None:
 
-    # upload updated manifest as artifact
-    dataset_task.upload_artifact(name = output_path, artifact_object = output_path)
-    # upload updated manifest as file
-    dataset.add_files(output_path)
+    use_project_names = len(parent_dataset_projects) > 0 and len(parent_dataset_names) > 0
+    if use_project_names:
+        extended_dataset_ids = get_parent_ids_by_names(
+            parent_dataset_projects=parent_dataset_projects, 
+            parent_dataset_names=parent_dataset_names)
+            
+        parent_dataset_ids.extend(extended_dataset_ids)
+        parent_dataset_ids = list(set(parent_dataset_ids))
 
-# upload dataset to remote storage
-dataset.upload(
-    output_url=OUTPUT_URI, 
-    verbose=True
-)
+    # intialize dataset task as current task
+    dataset = Dataset.create(
+        dataset_project = dataset_project,
+        dataset_name = dataset_name,
+        parent_datasets = parent_dataset_ids,
+    )
+    dataset_task = Task.get_task(task_id=dataset.id)
 
-# finalize the dataset
-dataset.finalize()
+    # pull artifacts from each parent dataset
+    artifact_syncs = {}
 
-task.set_parameter(
-    name='output_dataset_id', 
-    value=dataset.id, 
-    description='the dataset task id of the output dataset',
-) 
+    for parent_id in parent_dataset_ids:
+        parent_dataset_task = Task.get_task(task_id=parent_id)
 
-# end the task
-task.close()
+        for artifact_name in artifact_names:
+            if artifact_name not in artifact_syncs:
+                artifact_syncs[artifact_name] = []
+            # pull artifact and store in a list
+            artifact_path = parent_dataset_task.artifacts[artifact_name].get_local_copy()
+            # if file does not exist
+            if artifact_path is None:
+                continue
+            artifact_syncs[artifact_name].append(artifact_path)
+
+    # combine and upload artifacts
+    for artifact_name in artifact_names:
+        output_path  = artifact_name + '.json'
+        print(f'Combining {len(artifact_syncs[artifact_name])} files into: {output_path}')
+        with open(output_path, mode='w', encoding='utf-8') as fw:
+            # enter each input artifact and append each line to the new output artifact
+            for input_path in artifact_syncs[artifact_name]:
+                with open(input_path, mode='r', encoding='utf-8') as fr:
+                    lines = fr.readlines()
+                    for line in lines:
+                        fw.write(line)
+
+        # upload updated manifest as artifact
+        dataset_task.upload_artifact(name = output_path, artifact_object = output_path)
+        # upload updated manifest as file
+        dataset.add_files(output_path)
+
+    # upload dataset to remote storage
+    dataset.upload(
+        output_url=OUTPUT_URI, 
+        verbose=True
+    )
+
+    # finalize the dataset
+    dataset.finalize()
+
+    return dataset.id
+
+if __name__ == '__main__':
+
+    dataset_id = combine_datasets(
+        dataset_project = args['dataset_project'],
+        dataset_name = args['dataset_name'],
+        artifact_names = args['artifact_names'],
+        parent_dataset_ids = args['parent_dataset_ids'],
+        parent_dataset_projects = args['parent_dataset_projects'],
+        parent_dataset_names = args['parent_dataset_names']
+    )
+
+    task.set_parameter(
+        name='output_dataset_id', 
+        value=dataset_id, 
+        description='the dataset task id of the output dataset',
+    ) 
+
+    # end the task
+    task.close()
